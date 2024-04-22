0976fc525f0f9affc76992132d05a86191bbaea3
Bikash-Sasmal <bikash.sasmal.mca.2022@nist.edu>
2024-04-22 11:31:42 +0530

    Add a new React project otplogin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Heading from './components/Heading'
+import PhoneOtpForm from './components/phone-login'
+
+function App() {
+
+  return (
+    <div className='app'>
+     <Heading />
+     <PhoneOtpForm />
+    </div>
+  )
+}
+
+export default App
